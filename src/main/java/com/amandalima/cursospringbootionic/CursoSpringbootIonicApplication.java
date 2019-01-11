c3259a48f97c9af4f31099362aeeacc93e34d246
amandaisabelalima <amandaisabelalima@gmail.com>
2019-01-11 11:07:37 -0200

    created project

---
+++ src/main/java/com/amandalima/cursospringbootionic/CursoSpringbootIonicApplication.java
@@ -0,0 +1,14 @@
+package com.amandalima.cursospringbootionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringbootIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringbootIonicApplication.class, args);
+	}
+
+}
+
