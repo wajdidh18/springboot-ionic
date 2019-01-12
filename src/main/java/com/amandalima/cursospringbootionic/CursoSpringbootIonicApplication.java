0bdf65573b12120bdeff4151583b5eba1120f673
amandaisabelalima <amandaisabelalima@gmail.com>
2019-01-12 18:37:36 -0200

    Criando operacao de instanciacao

--- src/main/java/com/amandalima/cursospringbootionic/CursoSpringbootIonicApplication.java
+++ src/main/java/com/amandalima/cursospringbootionic/CursoSpringbootIonicApplication.java
@@ -1,13 +1,31 @@
 package com.amandalima.cursospringbootionic;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoSpringbootIonicApplication {
+import com.amandalima.cursospringbootionic.domain.Categoria;
+import com.amandalima.cursospringbootionic.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoSpringbootIonicApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringbootIonicApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
